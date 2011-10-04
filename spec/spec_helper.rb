36937fc23e30a664c9bf1c8055c47dcb8c0fd85a
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-04 13:50:22 +0200

    guard/rspec: init.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'rubygems'
+require 'rspec'
+
+# Require the library
+require 'watch_tower'
+
+include WatchTower
+
+# Require support files
+Dir[ROOT_PATH + "/spec/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  # config.mock_with :rspec
+end
