b9f56a665aa4b5a0821401fd17b38852c9a5cfda
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-04 14:42:42 +0200

    eye: removed..

--- lib/watch_tower/cli.rb
+++ lib/watch_tower/cli.rb
@@ -1,10 +1,8 @@
 require 'watch_tower/cli/thor'
-require 'watch_tower/cli/eye'
 
 module WatchTower
   module CLI
     class Runner < ::Thor::Group
-      include Eye
       include Thor
     end
   end
--- spec/watch_tower/cli/eye_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe CLI do
-  describe "Eye" do
-  end
-end
