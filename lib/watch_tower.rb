5ee4f99226d949935260984b240567229a0f2fbd
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-04 14:41:06 +0200

    Load active_support/core_ext/process/daemon before anything else, allows us to fork really quickly.

--- lib/watch_tower.rb
+++ lib/watch_tower.rb
@@ -1,5 +1,9 @@
 ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
+# Require daemon from active_support's core_ext allows us to fork really quickly
+require 'active_support/core_ext/process/daemon'
+
+# Require watch_tower's libraries
 require "watch_tower/version"
 require "watch_tower/core_ext"
 require "watch_tower/cli"
