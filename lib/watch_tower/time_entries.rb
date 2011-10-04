e89f988c447bd750898ca36eb102ed9b4db305c8
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-04 21:58:35 +0200

    travis: Bypass sqlite loading for Jruby on Travis.

--- lib/watch_tower/time_entries.rb
+++ lib/watch_tower/time_entries.rb
@@ -1,4 +1,13 @@
-require 'sqlite3'
+begin
+  require 'sqlite3'
+rescue LoadError => e
+  if ENV['TRAVIS']
+    # Travis hates sqlite with Jruby so it is disabled
+  else
+    raise LoadError, e
+  end
+end
+
 require 'active_record'
 
 module WatchTower
