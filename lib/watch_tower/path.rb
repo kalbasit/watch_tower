216a1041779a545d1a46a3a3376774aa74069f08
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-05 16:02:08 +0200

    Path: Added two protected methods one to find a project name and the other is to find a project's path

--- lib/watch_tower/path.rb
+++ lib/watch_tower/path.rb
@@ -6,15 +6,21 @@ module WatchTower
     #       They should be in a haml config file
     CODE_PATH = '~/Projects'
 
+    # Return the working directory (the project's path if you will) from a path
+    # to any file inside the project
+    #
+    # @param path The path to look the project path from
+    # @return [String] the project's folder
     def working_directory(path)
-
+      project_path_from_nested_path(path)
     end
 
     protected
 
-      # Find out the name of the project that's currently being worked on, under
-      # the code path, it uses the param nested_project_layers to determine the
-      # project name from the entire expanded path to any file under the project
+      # Find out the path parts of the project that's currently being worked on,
+      # under the code path, it uses the param nested_project_layers to determine
+      # the project name from the entire expanded path to any file under the
+      # project
       #
       # nested project layers works "how many folders inside your code folder
       # do you keep projects.
@@ -40,16 +46,39 @@ module WatchTower
       # @param code The path you store all the projects under
       # @param path The path to look the project name from
       # @param nested_project_layers How many folders, defaults to 2
-      # @return [String] The project's name
-      # @raise [WatchTower::PathError] if the path is not nested under code
-      def project_name_from_path(code, path, nested_project_layers = 2)
+      # @return [Array] The project path's parts
+      # @raise [WatchTower::PathNotUnderCodePath] if the path is not nested under code
+      def project_path_part(code, path, nested_project_layers = 2)
         regex_suffix = "([^/]+)"
         regex_suffix = [regex_suffix] * nested_project_layers
         regex_suffix = regex_suffix.join("/")
 
-        res = path.scan(%r{(#{code})/#{regex_suffix}}).flatten
-        raise PathNotUnderCodePath unless res.any?
-        res[nested_project_layers].chomp
+        path.scan(%r{(#{code})/#{regex_suffix}}).flatten.collect(&:chomp).
+          tap { |r| raise PathNotUnderCodePath unless r.any? }
+      end
+
+      # Find out the project's name
+      # See #project_path_part
+      #
+      # @param code The path you store all the projects under
+      # @param path The path to look the project name from
+      # @param nested_project_layers How many folders, defaults to 2
+      # @return [String] The project's name
+      # @raise [WatchTower::PathNotUnderCodePath] if the path is not nested under code
+      def project_name_from_nested_path(code, path, nested_project_layers = 2)
+        project_path_part(code, path, nested_project_layers)[nested_project_layers]
+      end
+
+      # Find out the project's path
+      # See #project_path_part
+      #
+      # @param code The path you store all the projects under
+      # @param path The path to look the project name from
+      # @param nested_project_layers How many folders, defaults to 2
+      # @return [String] The project's path
+      # @raise [WatchTower::PathNotUnderCodePath] if the path is not nested under code
+      def project_path_from_nested_path(code, path, nested_project_layers = 2)
+        project_path_part(code, path, nested_project_layers)[0..nested_project_layers].join('/')
       end
   end
 end
--- spec/watch_tower/path_spec.rb
+++ spec/watch_tower/path_spec.rb
@@ -2,26 +2,60 @@ require 'spec_helper'
 
 describe Path do
   before(:all) do
+    # Arguments
     @code = '/home/user/Code'
     @file_path = '/home/user/Code/OpenSource/watch_tower/lib/watch_tower/server/models/time_entries.rb'
     @nested_project_layers = 2
     @args = [@code, @file_path, @nested_project_layers]
 
+    # Expected results
+    @project_path = '/home/user/Code/OpenSource/watch_tower'
     @project_name = 'watch_tower'
+    @project_path_parts = ['/home/user/Code', 'OpenSource', 'watch_tower']
   end
 
-  describe "#project_name_from_path" do
-    it "should respond to #project_name_from_path" do
-      -> { subject.send :project_name_from_path, *@args }.should_not raise_error NoMethodError
+  describe "#project_path_part" do
+    it "should respond to #project_path_part" do
+      -> { subject.send :project_path_part, *@args }.should_not raise_error NoMethodError
     end
 
     it "should be able to return the project name from the path Given the correct path" do
-      subject.send(:project_name_from_path, *@args).should == @project_name
+      subject.send(:project_path_part, *@args).should == @project_path_parts
     end
 
-    it "should raises PathError if the path is not nested under code" do
+    it "should raises PathNotUnderCodePath if the path is not nested under code" do
       file_path = @file_path.gsub(%r{#{@code}}, '/some/other/path')
-      -> { subject.send(:project_name_from_path, @code, file_path, @nested_project_layers) }.should raise_error PathNotUnderCodePath
+      -> { subject.send(:project_path_part, @code, file_path, @nested_project_layers) }.should raise_error PathNotUnderCodePath
+    end
+  end
+
+  describe "#project_name_from_nested_path" do
+    it "should respond to #project_name_from_nested_path" do
+      -> { subject.send :project_name_from_nested_path, *@args }.should_not raise_error NoMethodError
+    end
+
+    it "should be able to return the project path parts from the path Given the correct path" do
+      subject.send(:project_name_from_nested_path, *@args).should == @project_name
+    end
+
+    it "should raises PathNotUnderCodePath if the path is not nested under code" do
+      file_path = @file_path.gsub(%r{#{@code}}, '/some/other/path')
+      -> { subject.send(:project_name_from_nested_path, @code, file_path, @nested_project_layers) }.should raise_error PathNotUnderCodePath
+    end
+  end
+
+  describe "#project_path_from_nested_path" do
+    it "should respond to #project_path_from_nested_path" do
+      -> { subject.send :project_path_from_nested_path, *@args }.should_not raise_error NoMethodError
+    end
+
+    it "should be able to return the project name from the path Given the correct path" do
+      subject.send(:project_path_from_nested_path, *@args).should == @project_path
+    end
+
+    it "should raises PathNotUnderCodePath if the path is not nested under code" do
+      file_path = @file_path.gsub(%r{#{@code}}, '/some/other/path')
+      -> { subject.send(:project_path_from_nested_path, @code, file_path, @nested_project_layers) }.should raise_error PathNotUnderCodePath
     end
   end
 end
