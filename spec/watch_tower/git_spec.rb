f292a0dff9b6b56757e4b25fe784d3be5909d6ed
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-05 14:01:40 +0200

    Git: Fix a bug.

--- spec/watch_tower/git_spec.rb
+++ spec/watch_tower/git_spec.rb
@@ -34,18 +34,40 @@ module WatchTower
       it { should respond_to(:active_for_path?) }
 
       it "should be able to determine if a path is git-ized" do
-        subject.active_for_path?(File.dirname(__FILE__)).should be_true
+        Git.expects(:git_folder_path).returns('/path/to/project')
+
+        subject.active_for_path?('/path/to/project/lib/file.rb').should be_true
+      end
+
+      it "should be able to determine if a path is not git-ized" do
+        Git.expects(:git_folder_path).returns(nil)
+
+        subject.active_for_path?('/path/to/project/lib/file.rb').should be_false
       end
     end
 
-    describe "#base_path_for_path" do
-      it { should respond_to(:base_path_for_path) }
+    describe "#working_directory" do
+      it { should respond_to(:working_directory) }
+
+      it "should return the working directory of a path" do
+        Git.expects(:active_for_path?).returns(true).once
+        Git.expects(:git_folder_path).returns('/path/to/project/.git').once
+
+        subject.working_directory('/path/to/project/lib/file.rb').should ==
+          '/path/to/project'
+      end
+
+      it "should return nil if none found" do
+        Git.expects(:active_for_path?).returns(false).once
+
+        subject.working_directory('/path/to/project/lib/file.rb').should be_nil
+      end
     end
 
     describe "#head" do
       before(:each) do
         ::WatchTower::Git.stubs(:active_for_path?).returns(true)
-        ::WatchTower::Git.stubs(:base_path_for_path).returns('/path/to/project')
+        ::WatchTower::Git.stubs(:working_directory).returns('/path/to/project')
       end
 
       it { should respond_to :head }
