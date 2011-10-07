520f62ed6a5c2c59f4f6f381d48b581e700786bf
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-07 16:11:26 +0200

    Server: Add a layout.

--- lib/watch_tower/server/app.rb
+++ lib/watch_tower/server/app.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 module WatchTower
   module Server
     class App < ::Sinatra::Application
+      get '/' do
+        haml :index
+      end
     end
   end
 end
--- spec/watch_tower/server/app_spec.rb
+++ spec/watch_tower/server/app_spec.rb
@@ -2,5 +2,19 @@ require 'spec_helper'
 
 module Server
   describe App do
+    before(:each) do
+      @project = FactoryGirl.create :project
+      @file = FactoryGirl.create :file, project: @project
+      @time_entry = FactoryGirl.create :time_entry, file: @file
+      @duration = FactoryGirl.create :duration, file: @file
+    end
+
+    describe "#index" do
+      it "should render the layout" do
+        visit '/'
+
+        page.should have_selector :xpath, '//html/head/title'
+      end
+    end
   end
 end
