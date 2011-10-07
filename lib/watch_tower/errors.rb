14005bb5dc8123e3d1d93d2ccd1794108a4f7dd1
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-07 12:31:20 +0200

    WatchTower: Update the documentation.

--- lib/watch_tower/errors.rb
+++ lib/watch_tower/errors.rb
@@ -2,19 +2,19 @@ module WatchTower
   # Global Error
   WatchTowerError = Class.new Exception
 
-  # Exceptions raised by the Project
+  # Exceptions raised by the Project module
   ProjectError = Class.new WatchTowerError
 
-  # Exception used by the Path module
+  # Exception raised by the Path module
   PathError = Class.new ProjectError
   PathNotUnderCodePath = Class.new PathError
 
-  # Exception used by the editors
+  # Exception raised by the Editor module
   EditorError = Class.new WatchTowerError
   TextmateError = Class.new EditorError
   XcodeError = Class.new EditorError
 
-  # Exceptions used by the server
+  # Exceptions raised by the Server module
   ServerError = Class.new WatchTowerError
   DatabaseError = Class.new ServerError
   DatabaseConfigNotFoundError = Class.new DatabaseError
