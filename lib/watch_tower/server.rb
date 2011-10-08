3f5e30104db4fcdfd4fee5fe6a5380c11758660c
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-08 13:36:38 +0200

    Server: The database nor the sinatra app should be launched on require, an explicit function should used to start them

--- lib/watch_tower/server.rb
+++ lib/watch_tower/server.rb
@@ -13,20 +13,25 @@ module WatchTower
     autoload :App
     autoload :Database
 
-    def self.start
-      begin
-        # Connect to the Database
-        Database.connect!
+    # Start the server
+    # This method starts the database and then starts the server
+    #
+    # @param [Hash] options
+    def self.start!(options = {})
+      # Start the Database
+      Database.start!(options)
 
-        # Migrate the database
-        Database.migrate!
-      rescue DatabaseConfigNotFoundError
-        STDERR.puts "Database configurations are missing, please edit #{Config::CONFIG_FILE} and try again."
-        exit(1)
-      rescue RuntimeError => e
-        STDERR.puts e
-        exit(1)
-      end
+      # Start the Sinatra application
+      start_web_server(options)
     end
+
+    protected
+      # Start the web_server
+      # This method starts the web server (The Sinatra app)
+      #
+      # @param [Hash] options
+      def self.start_web_server(options = {})
+        App.run!(options)
+      end
   end
 end
--- lib/watch_tower/server/database.rb
+++ lib/watch_tower/server/database.rb
@@ -1,27 +1,68 @@
-require 'active_record'
-
 module WatchTower
   module Server
     module Database
       extend self
 
-      def connect!
-        # Create a connection
-        ActiveRecord::Base.establish_connection(db_config)
-
-        # Create a looger
-        logger unless ENV['WATCH_TOWER_ENV'] == 'production'
-      end
-
-      def migrate!
-        # Connect to the database
+      # Start the database server
+      #
+      # see #connect!
+      # see #migrate!
+      # @param [Hash] options
+      def start!(options = {})
+        # Connect to the Database
         connect!
 
         # Migrate the database
-        ActiveRecord::Migrator.migrate(MIGRATIONS_PATH)
+        migrate!
+      rescue DatabaseConfigNotFoundError
+        STDERR.puts "Database configurations are missing, please edit #{Config::CONFIG_FILE} and try again."
+        exit(1)
+      rescue RuntimeError => e
+        STDERR.puts e
+        exit(1)
+      end
+
+      # Stop the database server
+      #
+      # see #disconnect!
+      # @param [Hash] options
+      def stop!(options = {})
+        # Disconnect from the database
+        disconnect!
+      rescue DatabaseConfigNotFoundError
+        STDERR.puts "Database configurations are missing, please edit #{Config::CONFIG_FILE} and try again."
+        exit(1)
+      rescue RuntimeError => e
+        STDERR.puts e
+        exit(1)
       end
 
       protected
+        # Connect to the database
+        def connect!
+          # Create a connection
+          ActiveRecord::Base.establish_connection(db_config)
+
+          # Create a looger
+          logger unless ENV['WATCH_TOWER_ENV'] == 'production'
+        end
+
+        # Disconnect from the database
+        # TODO: Implement this function
+        def disconnect!
+          raise NotImplementedError
+        end
+
+        # Migrate the database
+        def migrate!
+          # Connect to the database
+          connect!
+
+          # Migrate the database
+          ActiveRecord::Migrator.migrate(MIGRATIONS_PATH)
+        end
+
+        # Get the database configuration
         def db_config
           db_config = Config.try(:[], :database).try(:[], ENV['WATCH_TOWER_ENV'])
           raise DatabaseConfigNotFoundError unless db_config
@@ -31,10 +72,14 @@ module WatchTower
           db_config
         end
 
+        # Set the logger
         def logger
           ActiveRecord::Base.logger = Logger.new ::File.open(log_path, 'a')
         end
 
+        # Get the log file's absolute path
+        #
+        # @return [String] The database log file depending on the environment
         def log_path
           ::File.join(LOG_PATH, "#{ENV['WATCH_TOWER_ENV']}_database.log")
         end
