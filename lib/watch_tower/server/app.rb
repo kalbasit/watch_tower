7f59f01d8125bce599740f11d3f12d7d0fe2fb54
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-12 23:41:32 +0200

    Server: Record the date filtering in the session.

--- lib/watch_tower/server/app.rb
+++ lib/watch_tower/server/app.rb
@@ -16,12 +16,23 @@ module WatchTower
       paths :root => '/'
       paths :project => '/project/:id'
 
+      # Enable sessions
+      enable :sessions
+
       # The index action
       get :root do
         @title = "Projects"
+        # Parse the from/to date from params and add it to the session
         if params[:from_date] && params[:to_date]
+          session[:date_filtering] = {
+            from_date: params[:from_date],
+            to_date: params[:to_date]
+          }
+        end
+        # Either use date filtering or not
+        if session.try(:[], :date_filtering).try(:[], :from_date) && session.try(:[], :date_filtering).try(:[], :to_date)
           @projects = Project.
-            date_range(params[:from_date], params[:to_date]).
+            date_range(session[:date_filtering][:from_date], session[:date_filtering][:to_date]).
             worked_on
         else
           @projects = Project.worked_on
