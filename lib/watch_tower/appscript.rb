5c6b9c92247a02183404c23a5a79da5638fbd518
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-18 23:48:06 +0200

    Appscript: Remove the useless message it is now included in the Gemspec.

--- lib/watch_tower/appscript.rb
+++ lib/watch_tower/appscript.rb
@@ -1,13 +1,6 @@
 begin
-  require 'rubygems'
   require 'appscript'
 rescue LoadError
-  require 'rbconfig'
-  if RbConfig::CONFIG['target_os'] =~ /darwin/i
-    STDERR.puts "Please install 'appscript' to use this gem with Textmate"
-    STDERR.puts "gem install appscript"
-  end
-
   # Define a part of the Appscript gem so WatchTower is fully operational
   module ::Appscript
     CommandError = Class.new(Exception)
