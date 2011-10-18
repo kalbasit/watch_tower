3e3ffe562a3027fbb2b129e0ed4e1f05c879db2c
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-18 23:24:27 +0200

    WatchTower: Try to fix the gem for non-osx systems.

--- lib/watch_tower/appscript.rb
+++ lib/watch_tower/appscript.rb
@@ -7,24 +7,79 @@ rescue LoadError
     STDERR.puts "Please install 'appscript' to use this gem with Textmate"
     STDERR.puts "gem install appscript"
   end
-  # Define a simple class so the gem works even if Appscript is not installed
+
+  # Define a part of the Appscript gem so WatchTower is fully operational
   module ::Appscript
+    CommandError = Class.new(Exception)
+
     class Application
+
+      MOCK_METHODS = [:name, :version, :unix_id, :by_pid]
+
       def is_running?
         false
       end
 
-      def name
+      def get
         "Not available"
       end
 
-      def version
-        "Not available"
+      def processes
+        Process.new(self)
+      end
+
+      MOCK_METHODS.each do |method|
+        define_method method do |*args|
+          self
+        end
+      end
+    end
+
+    class Process
+      def initialize(klass)
+        @klass = klass
+      end
+
+      def [](*args)
+        [@klass]
       end
     end
 
     def app(*args)
       Application.new
     end
+
+    def its
+      self
+    end
+
+    def name
+      self
+    end
+
+    def eq(*args)
+      self
+    end
+
+    def Appscript.app(*args)
+      Application.new
+    end
+
+    def Appscript.its
+      self
+    end
+
+    def Appscript.name
+      self
+    end
+
+    def Appscript.eq(*args)
+      self
+    end
+
+  end
+
+  module ::FindApp
+    ApplicationNotFoundError = Class.new(Exception)
   end
 end
