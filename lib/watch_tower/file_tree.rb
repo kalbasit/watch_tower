f384e62fc118af566c60f35786631ba19d5f84b4
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-19 17:03:26 +0200

    FileTree: Change the class so it supports nested trees.

--- lib/watch_tower/file_tree.rb
+++ lib/watch_tower/file_tree.rb
@@ -3,7 +3,7 @@ module WatchTower
   # of the files and their elapsed time
   class FileTree
 
-    attr_reader :files
+    attr_reader :base_path, :files, :nested_tree, :elapsed_time
 
     # Initialize
     #
@@ -12,38 +12,65 @@ module WatchTower
     #                The Array elements should be Hashes with two required keys
     #                :path and :elapsed_time
     def initialize(base_path, files)
+      # Init with args
       @base_path = base_path
-      @files = files
+      @all_files = remove_base_path_from_files(@base_path, files)
+      # Init with defaults
       @elapsed_time = 0
-    end
-
-    # Render the FileTree
-    #
-    # @return [Array] The FileTree representation
-    def tree
-      # Remove base_path from paths
-      remove_base_path_from_paths
-      # Parse files
-      parse_files
-
-      # Return a hash
-      { files: files, elapsed_time: @elapsed_time }
+      @files = Array.new
+      @nested_tree = Hash.new
+      # Process the tree
+      process
     end
 
     protected
+      # Render the FileTree
+      #
+      # @return [Array] The FileTree representation
+      def process
+        # Parse files
+        parse_files
+        # Parse folders
+        parse_folders
+      end
+
       # Removes the base_path from the files
-      def remove_base_path_from_paths
-        @files.collect do |f|
-          f[:path].gsub!(%r(#{@base_path}#{File::SEPARATOR}), '')
+      def remove_base_path_from_files(base_path, files)
+        files.collect do |f|
+          f[:path] = f[:path].gsub(%r(#{base_path}#{File::SEPARATOR}), '')
+          f
         end
       end
 
       # Parses only files under the current base_path
       def parse_files
-        # Iterate over each file to calculate the total elapsed time
-        @files.each do |f|
+        return if @files.any?
+
+        @all_files.each do |f|
           unless f[:path] =~ %r(#{File::SEPARATOR})
             @elapsed_time += f[:elapsed_time]
+            @files << f
+          end
+        end
+      end
+
+      # Parses only folders under the current base_path
+      def parse_folders
+        return if @nested_tree.any?
+
+        @all_files.each do |f|
+          if f[:path] =~ %r(#{File::SEPARATOR})
+            # Get the base_path
+            base_path = f[:path].split(File::SEPARATOR).first
+            # Do not continue if we already parsed this path
+            next if @nested_tree.has_key?(base_path)
+            # Get the nested files
+            nested_files = remove_base_path_from_files base_path,
+              @all_files.select { |f| f[:path] =~ %r(^#{base_path}#{File::SEPARATOR}) }
+            # Create a tree
+            @nested_tree[base_path] = self.class.new("#{@base_path}#{File::SEPARATOR}#{base_path}", nested_files)
+            # Add the elapsed_time of the tree
+            @elapsed_time += @nested_tree[base_path].elapsed_time
           end
         end
       end
--- spec/watch_tower/file_tree_spec.rb
+++ spec/watch_tower/file_tree_spec.rb
@@ -21,50 +21,135 @@ describe FileTree do
     }
   end
 
-  subject { FileTree.new(@base_path, @files) }
+  describe "#remove_base_path_from_files" do
+    subject { FileTree.new(@base_path, @files) }
 
-  describe "#remove_base_path_from_paths" do
-    it { should respond_to :remove_base_path_from_paths }
+    it { should respond_to :remove_base_path_from_files }
 
     it "should remove the base_path from all the files in the @files array" do
-      subject.send :remove_base_path_from_paths
+      files = subject.send(:remove_base_path_from_files, @base_path, @simple_files)
 
-      subject.files.each do |f|
+      files.each do |f|
         f[:path].should_not match %r(#{@base_path}/)
       end
     end
   end
 
-  describe "#tree" do
-    it { should respond_to :tree }
+  describe "#process" do
+    subject { FileTree.new(@base_path, @files) }
 
-    it "should call remove_base_path_from_paths" do
-      subject.expects(:remove_base_path_from_paths).once
+    it { should respond_to :process }
 
-      subject.tree
+    it "should be called on initialize" do
+      FileTree.any_instance.expects(:process).once
+
+      FileTree.new(@base_path, @files)
     end
 
-    describe "simple files without folders" do
-      subject { FileTree.new(@base_path, @simple_files) }
+    it "should call parse_files" do
+      FileTree.any_instance.expects(:parse_files).once
+      FileTree.any_instance.stubs(:parse_folders)
 
-      it "should return a Hash" do
-        subject.tree.should be_instance_of Hash
-      end
+      FileTree.new(@base_path, @files)
+    end
 
-      it "should return a Hash containing an array identified by files" do
-        subject.tree[:files].should be_instance_of Array
+    it "should call parse_folders" do
+      FileTree.any_instance.stubs(:parse_files)
+      FileTree.any_instance.expects(:parse_folders).once
+
+      FileTree.new(@base_path, @files)
+    end
+  end
+
+  describe "simple files without folders" do
+    subject { FileTree.new(@base_path, @simple_files) }
+
+    describe "#files" do
+      it { should respond_to :files }
+
+      it "should return an array" do
+        subject.files.should be_instance_of Array
       end
 
       it "should include all the files with their elapsed times" do
-        subject.tree[:files].each do |f|
+        subject.files.each do |f|
           f.should be_instance_of Hash
           f[:path].should =~ /^file(1|2)\.rb$/
           f[:elapsed_time].to_s.should =~ /^(3600|1800)$/
         end
       end
+    end
+
+    describe "#elapsed_time" do
+      it { should respond_to :elapsed_time }
 
       it "should return elapsed_time" do
-        subject.tree[:elapsed_time].should == @simple_files_elapsed_times[@base_path]
+        subject.elapsed_time.should == @simple_files_elapsed_times[@base_path]
+      end
+    end
+
+    describe "#nested_tree" do
+      it { should respond_to :nested_tree }
+
+      it "should have an empty nested_tree" do
+        subject.nested_tree.should be_empty
+      end
+
+    end
+  end
+
+  describe "files with folders" do
+    subject { FileTree.new(@base_path, @files) }
+
+    describe "#files" do
+      it { should respond_to :files }
+
+      it "should return an array" do
+        subject.files.should be_instance_of Array
+      end
+
+      it "should include all the files with their elapsed times" do
+        subject.files.each do |f|
+          f.should be_instance_of Hash
+          f[:path].should =~ /^file(1|2)\.rb$/
+          f[:elapsed_time].to_s.should =~ /^(3600|1800)$/
+        end
+      end
+    end
+
+    describe "#elapsed_time" do
+      it { should respond_to :elapsed_time }
+
+      it "should return elapsed_time" do
+        subject.elapsed_time.should == @files_elapsed_times[@base_path]
+      end
+    end
+
+    describe "#nested_tree" do
+      it { should respond_to :nested_tree }
+
+      it "should not have an empty nested_tree" do
+        subject.nested_tree.should_not be_empty
+      end
+
+      it "should be a Hash" do
+        subject.nested_tree.should be_instance_of Hash
+      end
+
+      it "should return a FileTree for each element of the hash" do
+        subject.nested_tree['folder'].should be_instance_of FileTree
+      end
+
+      it "should include all the files under the folder" do
+        subject.nested_tree['folder'].files.each do |f|
+          f.should be_instance_of Hash
+          f[:path].should =~ /^file_under_folder(1|2)\.rb$/
+          f[:elapsed_time].to_s.should =~ /^(1800|900)$/
+        end
+      end
+
+      it "should return the elapsed time of the folder" do
+        subject.nested_tree['folder'].elapsed_time.should == @files_elapsed_times["#{@base_path}/folder"]
       end
     end
   end
