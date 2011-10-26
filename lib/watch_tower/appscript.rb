ca071bb32b7e2f8389abbeeb58529cb285e1012f
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-26 05:19:14 +0200

    Revert "Appscript: Remove the useless message it is now included in the Gemspec."
    This reverts commit 5c6b9c92247a02183404c23a5a79da5638fbd518.

--- lib/watch_tower/appscript.rb
+++ lib/watch_tower/appscript.rb
@@ -1,8 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 begin
+  require 'rubygems'
   require 'appscript'
 rescue LoadError
+  require 'rbconfig'
+  if RbConfig::CONFIG['target_os'] =~ /darwin/i
+    STDERR.puts "Please install 'appscript' to use this gem with Textmate"
+    STDERR.puts "gem install appscript"
+  end
+
   # Define a part of the Appscript gem so WatchTower is fully operational
   module ::Appscript
     CommandError = Class.new(Exception)
