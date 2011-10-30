34a87580513a1abda51f483674bd1155e3438bc2
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-30 22:30:37 +0100

    Spec: Speed up the test suite by 30%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 $:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+SPEC_PATH = File.expand_path(File.dirname(__FILE__))
 
 ENV['WATCH_TOWER_ENV'] = 'test'
 
@@ -11,10 +12,11 @@ require 'watch_tower'
 
 include WatchTower
 
-# Require support files
-Dir[ROOT_PATH + "/spec/support/**/*.rb"].each {|f| require f}
-
 RSpec.configure do |config|
+  def config.escaped_path(*parts)
+    Regexp.compile(parts.join('[\\\/]'))
+  end unless config.respond_to? :escaped_path
+
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -27,3 +29,6 @@ RSpec.configure do |config|
   # Treat symbols as metadata keys with true values
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
+
+# Require support files
+Dir[ROOT_PATH + "/spec/support/**/*.rb"].each {|f| require f}
